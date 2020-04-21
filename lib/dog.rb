7a4a7604116f23aee6f1af32555f77867fc320ca
tiya adugna <tiyakefalee@gmail.com>
2020-04-21 17:00:44 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,8 @@
 class Dog
+  def initialize(name)
+    @name = name 
+    @breed = "Mutt"
+  end
   def initialize(name, breed)
     @name = name 
    @breed = breed
@@ -11,10 +15,7 @@ class Dog
   def breed
     @breed 
   end
-  def initialize(name)
-    @name = name 
-    @breed = "Mutt"
-  end
+  
   
 end
  
